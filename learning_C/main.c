9bebe13d0e6a9e2667476c476dccc26558937d89
Boghost-alt <maxoumaxou1430@gmail.com>
2026-02-27 13:11:13 +0100

    Updated main.c

--- learning_C/main.c
+++ learning_C/main.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
-
-#define IN 1
-#define OUT 0
-
-int main(){
-    int pc,c,state;
-    state=OUT;
-    
-    while((c=getchar())!=EOF){
-        if(pc==' ' && c==' '||
-            pc==' ' && c=='\t'||
-            pc=='\t' && c==' '||
-            pc=='\t' && c=='\t'){}
-        else{
-            if((state==IN) && (c==' ' || c=='\t' || c=='\n')){
-                putchar('\n');
-                state=OUT;
-            }else if(state==OUT){
-                state=IN;
-                putchar(c);
-            }else putchar(c);
-        }
-        pc=c;
-    }
+/* count digits, white space, others */
+int main()
+{
+    int c, i, nwhite, nother;
+    int ndigit[10];
+    nwhite = nother = 0;
+    for (i = 0; i < 10; ++i)
+    ndigit[i] = 0;
+    while ((c = getchar()) != EOF)
+    if (c >= '0' && c <= '9')
+    ++ndigit[c-'0'];
+    else if (c == ' ' || c == '\n' || c == '\t')
+    ++nwhite;
+    else
+    ++nother;
+    printf("digits =");
+    for (i = 0; i < 10; ++i)
+    printf(" %d", ndigit[i]);
+    printf(", white space = %d, other = %d\n",
+    nwhite, nother);
 }
